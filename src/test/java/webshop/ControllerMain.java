43da887666f2a9b555da6390a759b94cf4c64b9e
Martinj98 <martin.dejongh@student.hu.nl>
2018-04-07 16:13:28 +0200

    tried fixing ojdbc for jenkins

--- src/test/java/webshop/ControllerMain.java
+++ src/test/java/webshop/ControllerMain.java
@@ -5,7 +5,8 @@ import webshop.domain.DomainController;
 public class ControllerMain {
 
 	public static void main(String[] args){
-		System.out.print("TESTCONTROLLERCLASS");
+		System.out.println("TESTCONTROLLERCLASS");
 		DomainController c= new DomainController();
+		System.out.println(c.getAllCategories());
 	}
 }
